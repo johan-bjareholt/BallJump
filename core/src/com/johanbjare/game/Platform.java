51436de667e31e1075c93fe3c69b7b7ce244b106
Johan Bjäreholt <johanthepro@gmail.com>
2015-03-17 19:22:32 +0100

    Made a high res sprite for long platforms

--- core/src/com/johanbjare/game/Platform.java
+++ core/src/com/johanbjare/game/Platform.java
@@ -5,19 +5,26 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Platform extends Sprite {
+
 	Platform(int x, int y)
 	{
-		super(new Texture("png/platform.png"),200,25);
-		if (y%1000==0){
+		super(new Texture("png/platform_long.png"));
+
+		if (y%1000==0) {
+			this.setTexture(new Texture("png/platform_long.png"));
 			this.setSize(Game.camera.viewportWidth,10);
 			x=0;
-			SpriteBatch textext = new SpriteBatch(this.getTexture().glTarget);
-			textext.begin();
-			Game.font_small.draw(textext, Float.toString(this.getY()) , this.getWidth()/2, 0);
-			textext.end();
+			//SpriteBatch textext = new SpriteBatch(this.getTexture().glTarget);
+			//textext.begin();
+			//Game.font_small.draw(textext, Float.toString(this.getY()) , this.getWidth()/2, 0);
+			//textext.end();
 		}
-		else
+		else {
+			this.setTexture(new Texture("png/platform.png"));
 			this.setSize(150, 10);
+		}
 		this.setPosition(x, y);
+		System.out.println(this.getTexture());
+		System.out.println(this.getY());
 	}
 }
